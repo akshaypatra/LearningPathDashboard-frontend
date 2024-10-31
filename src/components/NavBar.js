3b9922df8309ae70536c072bdd8d2a3106469972
Akshay Patra <akshay.patra114@gmail.com>
2024-10-31 12:57:47 +0530

    added a menu toggle button for smart phones

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-// import { SiSimpleanalytics } from "react-icons/si";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { GoHome } from "react-icons/go";
 import { FaUser } from "react-icons/fa6";
 import { RiLoginBoxLine } from "react-icons/ri";
 import { VscSignIn } from "react-icons/vsc";
+import { FiMenu } from "react-icons/fi";  // Icon for the menu button
 
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className='navbar'>
-        <h1 className='navbar-header'>Learning Path</h1>
-        <div className='navbar-links'>
-            <Link to='/teacher' className='navbar-link-dashboard'> <GoHome size={24}  className='react-icons' />Dashboard</Link> 
-            {/* <Link to='/student' className='navbar-link-dashboard'> <GoHome size={24}  className='react-icons' />Dashboard</Link>  */}
+      <h1 className='navbar-header'>Learning Path</h1>
+      <button className='menu-button' onClick={toggleMenu}>
+        <FiMenu size={24} />
+      </button>
+      <div className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+
+        {/* <Link to='/student' className='navbar-link-dashboard'> <GoHome size={24}  className='react-icons' />Dashboard</Link>  */}
             {/* <Link to='/analytics' className='navbar-link-dashboard'> <SiSimpleanalytics size={18}  className='react-icons' /> Analytics</Link> */}
-            <Link to='/profile' className='navbar-link-dashboard'> <FaUser size={18}  className='react-icons' /> Profile</Link>
-            <Link to='/login' className='navbar-link-dashboard'><RiLoginBoxLine className='react-icons'   />   Log In</Link>
-            <Link to='/signin' className='navbar-link-dashboard'><VscSignIn className='react-icons' />    Sign In</Link>
-            
-            
-        </div>
+        <Link to='/teacher' className='navbar-link-dashboard'> 
+          <GoHome size={24} className='react-icons' /> Dashboard
+        </Link>
+        <Link to='/profile' className='navbar-link-dashboard'>
+          <FaUser size={18} className='react-icons' /> Profile
+        </Link>
+        <Link to='/login' className='navbar-link-dashboard'>
+          <RiLoginBoxLine className='react-icons' /> Log In
+        </Link>
+        <Link to='/signin' className='navbar-link-dashboard'>
+          <VscSignIn className='react-icons' /> Sign In
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
+
+
+
